10b330874712196b16579123aaf82050c96a03a3
Marwa-Said <marwa_eltayeb@yahoo.com>
2018-10-08 23:29:35 +0200

    LogIn Form

--- app/src/main/java/com/marwa/eltayeb/LoginForm/MainActivity.java
+++ app/src/main/java/com/marwa/eltayeb/LoginForm/MainActivity.java
@@ -1,6 +1,7 @@
 package com.marwa.eltayeb.LoginForm;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,6 +12,8 @@ import android.view.MenuItem;
 
 import java.net.URL;
 
+import static com.marwa.eltayeb.LoginForm.LoginActivity.PREFS_NAME;
+
 public class MainActivity extends AppCompatActivity {
 
     Uri.Builder uriBuilder;
@@ -20,15 +23,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Intent intent = new Intent(this, LoginActivity.class);
-        //startActivity(intent);
+        //Intent intent = getIntent();
+        //String email = intent.getStringExtra("email2");
 
-        Intent intent = getIntent();
-        String email = intent.getStringExtra("email2");
+        SharedPreferences sPrefs = getSharedPreferences(PREFS_NAME, 0);
+        String receiveEmail = sPrefs.getString("storeEmail", "no mail");
 
         Uri baseUri = Uri.parse("http://192.168.1.5/learn/delete.php?");
         uriBuilder = baseUri.buildUpon();
-        uriBuilder.appendQueryParameter("email", email);
+        uriBuilder.appendQueryParameter("email", receiveEmail);
 
     }
 
@@ -43,15 +46,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // User clicked on a menu option in the app bar overflow menu
+
         switch (item.getItemId()) {
+            // Sign Out
+            case R.id.sign_out:
+                SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.remove("logged");
+                editor.apply();
+                Intent intent1 = new Intent(this, LoginActivity.class);
+                startActivity(intent1);
+                break;
+
             // Delete Account
             case R.id.delete_account:
                 // Kick off an {@link AsyncTask} to perform the network request
                 LogInAsyncTask task = new LogInAsyncTask();
                 task.execute();
-                Intent intent = new Intent(this, SignUpActivity.class);
-                startActivity(intent);
-                return true;
+                SharedPreferences prefs = getSharedPreferences(PREFS_NAME, 0);
+                SharedPreferences.Editor editor2 = prefs.edit();
+                editor2.remove("logged");
+                editor2.apply();
+                MainActivity.this.finish();
+                Intent intent2 = new Intent(this, SignUpActivity.class);
+                startActivity(intent2);
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
